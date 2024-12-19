649fed144dce97be3cdd47a0634b800548304d9e
Jesús Santos Capote <jesussantoscapote@gmail.com>
2024-12-19 12:22:56 +0100

    add views to reservation

--- reservation_system/api/views.py
+++ reservation_system/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from .models import Client, Reservation
 from .serializer import ClientSerializer, ReservationSerializer
 
-
+# Reservation Endpoints
 class ReservationListView(ListAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
@@ -15,6 +15,21 @@ class ReservationCreateView(CreateAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
+
+class ReservationDestroyView(DestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class ReservationRetrieveView(RetrieveAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class ReservationUpdateView(UpdateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    
 
 # Client Endpoints
 class ClientCreateView(CreateAPIView):
