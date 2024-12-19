3d24bae2afcd19e3d2f6446e699a5108cbcc988b
Jesús Santos Capote <jesussantoscapote@gmail.com>
2024-12-19 12:42:24 +0100

    add tests to reservation endpoints

--- reservation_system/api/tests.py
+++ reservation_system/api/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from rest_framework import status, reverse
+from rest_framework.test import APITestCase
+from .models import Client, Reservation
+from .serializer import ClientSerializer, ReservationSerializer
 
-# Create your tests here.
+class ReservationViewsTests(APITestCase):
+    def setUp(self):
+        self.client_data = {
+            'name': 'John Doe',
+            'email': 'john@example.com',
+        }
+        self.client_obj = Client.objects.create(**self.client_data)
+        
+        self.reservation_data = {
+            'client': self.client_obj.id,
+            'date': '2024-12-19',
+            'status': 'PENDING'
+        }
+        self.reservation = Reservation.objects.create(**self.reservation_data)
+
+    def test_reservation_list(self):
+        url = reverse('get-reservations')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_reservation_create(self):
+        url = reverse('create-reservation')
+        new_reservation = {
+            'client': self.client_obj.id,
+            'date': '2024-12-20',
+            'status': 'PENDING'
+        }
+        response = self.client.post(url, new_reservation, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Reservation.objects.count(), 2)
+
+    def test_reservation_retrieve(self):
+        url = reverse('detail-reservation', args=[self.reservation.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['client'], self.client_obj.id)
+
+    def test_reservation_update(self):
+        url = reverse('update-reservation', args=[self.reservation.id])
+        updated_data = {
+            'client': self.client_obj.id,
+            'date': '2024-12-20',
+            'status': 'CANCELED'
+        }
+        response = self.client.put(url, updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status'], 'CANCELED')
+
+    def test_reservation_delete(self):
+        url = reverse('delete-reservation', args=[self.reservation.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Reservation.objects.count(), 0)
--- reservation_system/api/urls.py
+++ reservation_system/api/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from .views import ReservationListView, ReservationCreateView, ReservationUpdateView, ReservationDestroyView, ReservationRetrieveView, ClientListView, ClientCreateView, ClientUpdateView, ClientRetrieveView, ClientDestroyView
 
 urlpatterns = [
-    path('reservations/', ReservationListView.as_view(), name='get_reservations'),
-    path('reservations/create/', ReservationCreateView.as_view(), name='create_reservation'),
-    path('reservations/<int:pk>/update', ReservationUpdateView.as_view(), name='update_reservation'),
-    path('reservations/<int:pk>/delete', ReservationDestroyView.as_view(), name='delete_reservation'),
-    path('reservations/<int:pk>/', ReservationRetrieveView.as_view(), name='update_reservation'),
-    path('clients/', ClientListView.as_view(), name= 'get_clients'),
-    path('clients/create/', ClientCreateView.as_view(), name = 'create_client'),
-    path('clients/<int:pk>/update', ClientUpdateView.as_view(), name = 'update_client'),
-    path('clients/<int:pk>/', ClientRetrieveView.as_view(), name = 'get_client'),
-    path('clients/<int:pk>/delete', ClientDestroyView.as_view(), name = 'delete_client')
+    path('reservations/', ReservationListView.as_view(), name='get-reservations'),
+    path('reservations/create/', ReservationCreateView.as_view(), name='create-reservation'),
+    path('reservations/<int:pk>/update', ReservationUpdateView.as_view(), name='update-reservation'),
+    path('reservations/<int:pk>/delete', ReservationDestroyView.as_view(), name='delete-reservation'),
+    path('reservations/<int:pk>/', ReservationRetrieveView.as_view(), name='detail-reservation'),
+    path('clients/', ClientListView.as_view(), name= 'get-clients'),
+    path('clients/create/', ClientCreateView.as_view(), name = 'create-client'),
+    path('clients/<int:pk>/update', ClientUpdateView.as_view(), name = 'update-client'),
+    path('clients/<int:pk>/', ClientRetrieveView.as_view(), name = 'get-client'),
+    path('clients/<int:pk>/delete', ClientDestroyView.as_view(), name = 'delete-client')
 ]
