d9e65e817ff9d3474aaddad323f8e95cdcd65bb0
Jesús Santos Capote <jesussantoscapote@gmail.com>
2024-12-19 11:33:01 +0100

    add views and serilizer for clients and reservations

--- reservation_system/api/views.py
+++ reservation_system/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework import status
+from .models import Client, Reservation
+from .serializer import ClientSerializer, ReservationSerializer
 
-# Create your views here.
+
+class ReservationListView(ListAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class ReservationCreateView(CreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+# Client Endpoints
+class ClientCreateView(CreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ClientListView(ListAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ClientDestroyView(DestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ClientUpdateView(UpdateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ClientRetrieveView(RetrieveAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
