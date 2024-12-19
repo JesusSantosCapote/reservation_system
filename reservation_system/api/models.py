0c821a885f84f484961039974150597d2742d8c0
Jesús Santos Capote <jesussantoscapote@gmail.com>
2024-12-19 11:19:00 +0100

    add client model and reservation model

--- reservation_system/api/models.py
+++ reservation_system/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from enum import Enum
+
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ReservationStatus(Enum):
+    PENDING = "PENDING"
+    IN_PROGRESS = "IN_PROGRESS"
+    COMPLETED = "COMPLETED"
+    CANCELED = "CANCELED"
+    NO_SHOW = "NO_SHOW"
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+
+class Reservation(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    reservation_date = models.DateField()
+    status = models.CharField(max_length=100, choices=ReservationStatus.choices())
